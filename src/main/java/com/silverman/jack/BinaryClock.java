664ccdc22cd6c19f932caf8671013fdb73143a4e
Jack Silverman <Jackdsilverman@gmail.com>
2019-05-16 18:37:15 -0400

    code update

--- src/main/java/com/silverman/jack/BinaryClock.java
+++ src/main/java/com/silverman/jack/BinaryClock.java
@@ -3,15 +3,21 @@ package com.silverman.jack;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.RaspiPin;
+
 public class BinaryClock {
-//	final GpioController gpio;
-//	private Pin[] pins = new Pin[] { RaspiPin.GPIO_14, RaspiPin.GPIO_15, RaspiPin.GPIO_18, RaspiPin.GPIO_23,
-//			RaspiPin.GPIO_11, RaspiPin.GPIO_05, RaspiPin.GPIO_06, RaspiPin.GPIO_24, RaspiPin.GPIO_25, RaspiPin.GPIO_08,
-//			RaspiPin.GPIO_07, RaspiPin.GPIO_13, RaspiPin.GPIO_19, RaspiPin.GPIO_26, RaspiPin.GPIO_12, RaspiPin.GPIO_16,
-//			RaspiPin.GPIO_20, RaspiPin.GPIO_21 };
+	final GpioController gpio;
+	private Pin[] pins = new Pin[] { RaspiPin.GPIO_14, RaspiPin.GPIO_15, RaspiPin.GPIO_18, RaspiPin.GPIO_23,
+			RaspiPin.GPIO_11, RaspiPin.GPIO_05, RaspiPin.GPIO_06, RaspiPin.GPIO_24, RaspiPin.GPIO_25, RaspiPin.GPIO_08,
+			RaspiPin.GPIO_07, RaspiPin.GPIO_13, RaspiPin.GPIO_19, RaspiPin.GPIO_26, RaspiPin.GPIO_12, RaspiPin.GPIO_16,
+			RaspiPin.GPIO_20, RaspiPin.GPIO_21 };
 
 	public BinaryClock() {
-//		gpio = GpioFactory.getInstance();
+		gpio = GpioFactory.getInstance();
 	}
 
 	private String getTime() {
@@ -41,15 +47,15 @@ public class BinaryClock {
 
 	public void displayBinaryTime() {
 		StringBuilder binaryTime = convertToBinary();
-//		for (int i = 0; i < pins.length; i++) {
-//			if (binaryTime.charAt(i) == '1') {
-//				GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(pins[i]);
-//				led.high();
-//			} else if (binaryTime.charAt(i) == '0') {
-//				GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(pins[i]);
-//				led.low();
-//			}
-//		}
+		for (int i = 0; i < pins.length; i++) {
+			if (binaryTime.charAt(i) == '1') {
+				GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(pins[i]);
+				led.high();
+			} else if (binaryTime.charAt(i) == '0') {
+				GpioPinDigitalOutput led = gpio.provisionDigitalOutputPin(pins[i]);
+				led.low();
+			}
+		}
 
 	}
 }
